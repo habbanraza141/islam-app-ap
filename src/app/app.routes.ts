c825dd66e0b544ae4ec9df60949c6bfd99ec1d3d
habbanraza141 <muhammadhabbanraza@gmail.com>
2025-04-29 23:37:29 +0500

    routing updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { CategoriesComponent } from './front-end/categories/categories.component
 import { LanguagesComponent } from './front-end/languages/languages.component';
 import { AuthorsComponent } from './front-end/authors/authors.component';
 import { authGuard } from './shared/guard/auth.guard';
+import { CarouselImagesComponent } from './front-end/carousel-images/carousel-images.component';
+import { AdvertisementComponent } from './front-end/advertisement/advertisement.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -13,11 +15,13 @@ export const routes: Routes = [
     { path: '', component: DashboardComponent,
         canActivate: [authGuard],
         children: [
-            { path: '', redirectTo: 'books', pathMatch: 'full' }, // Redirect to books by default
+            { path: '', redirectTo: 'books', pathMatch: 'full' }, 
             { path: 'books', component: BookAdminComponent },
             { path: 'categories', component: CategoriesComponent },
             { path: 'languages', component: LanguagesComponent },
             { path: 'authors', component: AuthorsComponent },
+            { path: 'carousel', component: CarouselImagesComponent },
+            { path: 'ad', component: AdvertisementComponent },
         ] 
      },
 
