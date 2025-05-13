ec785ef46c8699961c9b6d33835033247b6ea533
habbanraza141 <muhammadhabbanraza@gmail.com>
2025-05-13 20:00:18 +0500

    swal implimented

--- src/app/front-end/carousel-images/carousel-images.component.ts
+++ src/app/front-end/carousel-images/carousel-images.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CarouselService } from '../../shared/services/carousel.service';
+import {
+  getDownloadURL,
+  ref,
+  Storage,
+  uploadBytes,
+} from '@angular/fire/storage';
+import Swal from 'sweetalert2';
 
 interface Carousel {
   id: number;
@@ -15,12 +22,14 @@ interface Carousel {
   styleUrl: './carousel-images.component.css',
 })
 export class CarouselImagesComponent implements OnInit {
-  constructor(private carouselService: CarouselService) {}
-
+  constructor(
+    private carouselService: CarouselService,
+    private storage: Storage
+  ) {}
+  selectedFile: File | null = null;
   carouselContainer: any[] = [];
   editIndex: number | null = null;
   carousel: Carousel = {
-    
     id: 0,
     image: '',
   };
@@ -35,26 +44,127 @@ export class CarouselImagesComponent implements OnInit {
     });
   }
 
-  submitForm() {
-    // const carouselData = { ...this.carousel };
-    // console.log(carouselData);
-    // this.carouselService.addCarousel(carouselData).subscribe({
-    //   next: () => {
-    //     this.carouselContainer.push(carouselData); // Optionally update the UI after successful submission
-    //     this.resetForm();
-    //   },
-    //   error: (err) => console.error('Error submitting category:', err),
-    // });
+  async submitForm() {
+    if (!this.selectedFile) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'No Image Selected',
+        text: 'Please select an image before submitting.',
+      });
+      return;
+    }
+
+    const result = await Swal.fire({
+      title: 'Are you sure?',
+      text: 'Do you want to submit this advertisement?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, submit it!',
+      cancelButtonText: 'Cancel',
+    });
+
+    if (!result.isConfirmed) {
+      return;
+    }
+
+    try {
+      const filePath = `carousel_images/${Date.now()}_${
+        this.selectedFile.name
+      }`;
+      const storageRef = ref(this.storage, filePath);
+      const uploadResult = await uploadBytes(storageRef, this.selectedFile);
+      const downloadURL = await getDownloadURL(uploadResult.ref);
+
+      const newCarousel: Carousel = {
+        id: Date.now(), 
+        image: downloadURL,
+      };
+
+      this.carouselService.addCarousel(newCarousel).subscribe({
+        next: () => {
+          this.carouselContainer.push(newCarousel);
+          this.resetForm();
+          Swal.fire({
+            icon: 'success',
+            title: 'Submitted!',
+            text: 'Your carousel has been added.',
+          });
+        },
+        error: (err) => {
+          console.error('Error submitting carousel:', err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Submission Failed',
+            text: 'There was an error saving the carousel.',
+          });
+        },
+      });
+    } catch (error) {
+      console.error('Upload error:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Upload Failed',
+        text: 'Something went wrong while uploading the image.',
+      });
+    }
   }
 
-  editCarousel(index: number) {}
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
 
-  deleteCarousel(index: number) {}
+  editCarousel(index: number) {
+    this.editIndex = index;
+    const carouselToEdit = this.carouselContainer[index];
+    this.carousel = { ...carouselToEdit };
+  }
+
+  deleteCarousel(index: number) {
+    const imageToDelete = this.carouselContainer[index];
+
+    if (!imageToDelete.docId) {
+      console.error('No Firestore document ID found for this image.');
+      return;
+    }
+
+    Swal.fire({
+      title: `Are you sure you want to delete this image"?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.carouselService.deleteCarousel(imageToDelete.docId).subscribe({
+          next: () => {
+            this.carouselContainer.splice(index, 1);
+            Swal.fire({
+              icon: 'success',
+              title: 'Deleted!',
+              text: `Image was deleted successfully.`,
+              timer: 1000,
+              showConfirmButton: false,
+            });
+          },
+          error: (err) => {
+            console.error('Error deleting image:', err);
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'There was a problem deleting the image. Please try again later.',
+            });
+          },
+        });
+      }
+    });
+  }
 
   resetForm() {
     this.carousel = {
       id: 0,
-      image: ''
+      image: '',
     };
   }
 }
